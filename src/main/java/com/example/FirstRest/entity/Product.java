18a20e60a6a34665067e5461d04bb9cd0ee143da
TempUse24 <hellotemp2024@gmail.com>
2024-07-31 10:00:08 +0530

    Add files via upload

---
+++ src/main/java/com/example/FirstRest/entity/Product.java
@@ -0,0 +1,21 @@
+package com.example.FirstRest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	
+	@Id
+	private long productId;
+	private String description;
+	private double price;
+	
+	
+}
